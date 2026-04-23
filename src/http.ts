fdf4df8d961c9bab34357efae2cc9c7055d5b9b4
Nasrudin Salim <nasrudin.salim.suden@gmail.com>
2026-04-23 22:11:36 +0800

    release: v1.4.3

--- src/http.ts
+++ src/http.ts
@@ -30,7 +30,7 @@ export class HTTPClient {
     this.headers = {
       Authorization: `Bearer ${options.apiKey}`,
       "Content-Type": "application/json",
-      "User-Agent": "sonzai-typescript/1.4.1",
+      "User-Agent": "sonzai-typescript/1.4.3",
       ...options.defaultHeaders,
     };
     this.timeout = options.timeout;
